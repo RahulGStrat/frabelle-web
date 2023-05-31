eb4d4f3e829b11e823090bc70b957c66b2ca84cd
Sanjay <sanjay.e@strat-agile.com>
2023-05-31 14:52:33 +0530

    testiomonials

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,10 +20,43 @@ window.addEventListener('resize', () => {
 })
 
 // testimonials
-$('.fb-customer__outer').slick({
+// $('.fb-customer__outer').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3
+// });
+$(".fb-customer__outer").slick({
+    dots: true,
     infinite: true,
+    speed: 800,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2800,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+            },
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+    ],
 });
 //   main banner start
 $(".fb-MainBanner__tile-wrap").slick({
