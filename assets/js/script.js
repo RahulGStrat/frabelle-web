b3808e2b0e70ac9f1a06f6ada4607c4756aa4005
rigeshKR <rigeshkumar@gmail.com>
2023-05-31 12:49:45 +0530

    banner updated

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+//   main banner start
+$(".fb-MainBanner__tile-wrap").slick({
+  dots: true,
+  infinite: true,
+  speed: 800,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2800,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+//   Main banner end
